bafadf555d7ecfb648b1a8abb3134b7d5732ef00
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-03-01 17:50:21 -0800

    updated concole methods to use the commands with every class name. pennding to fix all *className* that prints a comma in the last iteration

--- console.py
+++ console.py
@@ -6,6 +6,12 @@ console.py module which contains the console
 import cmd
 import sys
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 from models import storage
 
 
@@ -15,6 +21,7 @@ class HBNBCommand(cmd.Cmd):
     """
 
     prompt = '(hbnb) '
+    classList = ["BaseModel", "City", "State", "Place", "Review", "User", "Amenity"]
 
     def emptyline(self):
         """overwritted method to avoid a message if the command line is empty"""
@@ -38,8 +45,8 @@ class HBNBCommand(cmd.Cmd):
         """
         if arg == "":
             print("** class name missing **")
-        elif arg == "BaseModel":
-            new = BaseModel()
+        elif arg in self.classList:
+            new = eval(arg)()
             new.save()
             print(new.id)
         else:
@@ -50,25 +57,24 @@ class HBNBCommand(cmd.Cmd):
         Prints str of an instance Ex: $ show BaseModel 1234-1234-1234
         """
         args = arg.split()
-        jsLoaded = storage.reload()
+        jsLoaded = storage.all()
+        flag = 0
         if arg == "":
             print("** class name missing **")
         else:
+            print(f'jsLoaded es: \n{jsLoaded}')
             for obj in jsLoaded:
                 #making a list with the class name of the object and their id
                 objAndIdList = obj.split('.')
-                if args[0] != objAndIdList[0]:
+                if args[0] not in self.classList:
                     print("** class doesn't exist **")
                 else:
                     if len(args) == 1:
                         print("** instance id missing **")
                     else:
-                        flag = 0
-                        for attr in jsLoaded[obj]:
-                            if jsLoaded[obj][attr] == args[1]:
-                                flag = 1
-                                toPrint = BaseModel(**jsLoaded[obj])
-                                print(toPrint)
+                        if args[1] == objAndIdList[1]:
+                            flag = 1
+                            print(jsLoaded[obj])
             if flag == 0:
                 print("** no instance found **")
 
@@ -77,26 +83,25 @@ class HBNBCommand(cmd.Cmd):
         Deletes an instance based on the class name and id
         """
         args = arg.split()
-        jsLoaded = storage.reload()
+        jsLoaded = storage.all()
+        flag = 0
         if arg == "":
             print("** class name missing **")
         else:
-            for obj in jsLoaded:
+            for obj in jsLoaded.keys():
                 #making a list with the class name of the object and their id
                 objAndIdList = obj.split('.')
-                if args[0] != objAndIdList[0]:
+                if args[0] not in self.classList:
                     print("** class doesn't exist **")
                 else:
                     if len(args) == 1:
                         print("** instance id missing **")
                     else:
-                        flag = 0
-                        for attr in jsLoaded[obj]:
-                            if jsLoaded[obj][attr] == args[1]:
-                                flag = 1
-                                jsLoaded.pop(obj)
-                                storage.save()
-                                return
+                        if objAndIdList[1] == args[1]:
+                            flag = 1
+                            jsLoaded.pop(obj)
+                            storage.save()
+                            return
             if flag == 0:
                 print("** no instance found **")
 
@@ -104,31 +109,38 @@ class HBNBCommand(cmd.Cmd):
         """
         Prints string representation of all instances
         """
-        jsLoaded = storage.reload()
+        jsLoaded = storage.all()
         flag = 0
+        i = 0
         for obj in jsLoaded:
             #making a list with class name of the object and their id
             objAndIdList = obj.split('.')
-            if arg != "" and arg != objAndIdList[0]:
+            if arg != "" and arg not in self.classList:
                 print("** class doesn't exist **")
                 return
-            elif arg == objAndIdList[0]:
-                toPrint = BaseModel(**jsLoaded[obj])
-                print(f'["{toPrint}"]')
-                return
-            else:
-                toPrint = BaseModel(**jsLoaded[obj])
+            elif arg != "" and arg == objAndIdList[0]:
                 if flag == 0:
-                    print("[\"",end='')
+                    print("[\"", end='')
                     flag = 1
-                print(toPrint,end='')
+                print(jsLoaded[obj],end='')
+                if flag == 1 and i < len(jsLoaded) - 1:
+                    print(", ", end='')
+                i += 1
+            if arg == "":
+                if flag == 0:
+                    print("[\"", end='')
+                    flag = 1
+                print(jsLoaded[obj],end='')
+                if flag == 1 and i < len(jsLoaded) - 1:
+                    print(", ", end='')
+                i += 1
         print("\"]")
 
     def do_update(self, arg):
         """
         Updates an specific instance by adding or updating attribute
         """
-        jsLoaded = storage.reload()
+        jsLoaded = storage.all()
         args = arg.split()
         flag = 0
         if arg == "":
@@ -136,25 +148,22 @@ class HBNBCommand(cmd.Cmd):
         for obj in jsLoaded:
             #making a list with the class name of the object and their id
             objAndIdList = obj.split('.')
-            if args[0] != objAndIdList[0]:
+            if args[0] not in self.classList:
                 print("** class doesn't exist **")
             else:
                 if len(args) == 1:
                     print("** instance id missing **")
                 else:
-                    for attr in jsLoaded[obj]:
-                        if len(args) == 2:
-                            print("** attribute name missing **")
+                    if len(args) == 2:
+                        print("** attribute name missing **")
+                    else:
+                        if len(args) == 3:
+                            print("** value missing **")
                         else:
-                            if len(args) == 3:
-                                print("** value missing **")
-                            else:
-                                if jsLoaded[obj][attr] == args[1]:
-                                    flag = 1
-                                    jsLoaded[obj][args[2]] = args[3].strip('"')
-                                    storage.save()
-                                    return
-
+                            if objAndIdList[1] == args[1]:
+                                flag = 1
+                                setattr(jsLoaded[obj], args[2], args[3].strip('"'))
+                                storage.save()
         if flag == 0:
             print("** no instance found **")
 
