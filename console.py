b0bce26e008f55c53903f8ec659c50bed529fcb9
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-03-02 12:47:22 -0800

    added fuctions of advanced excersices. we must to fix bugs

--- console.py
+++ console.py
@@ -35,12 +35,14 @@ class HBNBCommand(cmd.Cmd):
         """
         quit method which raises End Of File
         """
+        print("")
         return True
 
     def do_EOF(self, arg):
         """
         EOF method which raises End Of File
         """
+        print("")
         return True
 
     def do_create(self, arg):
@@ -176,6 +178,65 @@ class HBNBCommand(cmd.Cmd):
             print("** no instance found **")
             return
 
+
+    def do_count(self, arg):
+        """
+        Prints str of an instance Ex: $ show BaseModel 1234-1234-1234
+        """
+        args = arg.split()
+        jsLoaded = storage.all()
+        i = 0
+        if arg == "":
+            print("** class name missing **")
+            return
+        else:
+            for obj in jsLoaded:
+                # making a list with the class name of the object and their id
+                objAndIdList = obj.split('.')
+                if args[0] not in self.classList:
+                    print("** class doesn't exist **")
+                    return
+                else:
+                    if args[0] == objAndIdList[0]:
+                        i += 1
+            print(i)
+
+    def default(self, arg):
+        """
+        Method called on an input line when the command prefix
+        is not recognized. If this method is not overridden,
+        it prints an error message and returns.
+        """
+        Rarg = arg.strip(')')
+        Rarg = Rarg.strip('(')
+        Rarg = Rarg.replace("\"",".")
+        args = Rarg.split('.')
+        try:
+            # User.all()
+            # User.count()
+            try:
+                eval(f'self.do_{args[1]}')(args[0])
+            except Exception:
+                args[1] = args[1].strip('(')
+                if len(args) == 4:
+                    print(f'len: {len(args)}')
+                    eval(f'self.do_{args[1]}')(f'{args[0]} {args[2]}')
+                else:
+                    Rarg = arg.split('.')
+                    if len(Rarg) != 2:
+                        return
+                    Rarg[1] = Rarg[1].replace("(", ",")
+                    Larg = Rarg[1].split(',')
+                    Larg[0] = Larg[0].strip('"')
+                    Larg[1] = Larg[1].strip(' ')
+                    Larg[1] = Larg[1].strip('"')
+                    Larg[2] = Larg[2].strip(' ')
+                    Larg[2] = Larg[2].strip('"')
+                    Larg[3] = Larg[3].strip(' ')
+                    Larg[3] = Larg[3].strip(')')
+                    eval(f'self.do_{Larg[0]}')(f'{Rarg[0]} {Larg[1]} {Larg[2]} {Larg[3]}')
+        except Exception as a:
+            print(f'*** Unknown syntax: {arg}')
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
