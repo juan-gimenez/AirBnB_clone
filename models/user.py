1d19de983c93918ed55e992f4cdca95240ccd989
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-03-05 16:41:35 -0800

    fixed class user

--- models/user.py
+++ models/user.py
@@ -12,9 +12,7 @@ class User(BaseModel):
     User module which manages the users of the system
     """
 
-    def __init__(self, *args, **kwargs):
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
-        super().__init__(*args, **kwargs)
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
